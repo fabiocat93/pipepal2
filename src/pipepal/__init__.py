e1449909d9e21313fb3790e8d1c443eabed9c161
fabiocat93 <fabio-cat93@hotmail.it>
2024-03-25 19:08:33 -0400

    playing with ruff

--- src/pipepal/__init__.py
+++ src/pipepal/__init__.py
@@ -1,1 +1,4 @@
+"""Main package for pipepal."""
 from .app import hello_world
+
+__all__ = ['hello_world']
--- src/pipepal/app.py
+++ src/pipepal/app.py
@@ -1,3 +1,8 @@
+"""This script defines a simple function that prints 'Hello World!' to the console.
+
+It then calls this function if the script is run as the main program.
+"""
+
 def hello_world() -> None:
     """Prints hello world."""
     print("Hello World!")
