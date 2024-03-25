207a74f7922a2212621af928a00cc2c354821b13
fabiocat93 <fabio-cat93@hotmail.it>
2024-03-25 19:46:23 -0400

    playing with mypy

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,9 @@
 """Tests for the app hello_world function."""
 from pipepal import hello_world
+from _pytest.capture import CaptureFixture
 
 
-def test_hello_world(capsys: object) -> None:  
+def test_hello_world(capsys: CaptureFixture) -> None:  
     """A test function for hello_world.
 
     This function uses the capsys fixture to capture stdout and stderr.
