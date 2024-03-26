cfa1e04b4362294876d85a7b79b24d07bcf00bbb
fabiocat93 <fabio-cat93@hotmail.it>
2024-03-26 18:14:31 -0400

    adding readme.md to pdoc

--- src/pipepal/__init__.py
+++ src/pipepal/__init__.py
@@ -3,4 +3,8 @@ from .app import hello_world
 
 __all__ = ['hello_world']
 
-""".. include:: ../../README.md"""
+"""
+This is the content of the README.md file.
+"""
+with open('../../README.md', 'r') as file:
+    __doc__ = file.read()
